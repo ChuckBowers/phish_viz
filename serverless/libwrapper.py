dcad0fb89ee5e19d5dfcf1687df1d89035904948
wowers <bowerswill@gmail.com>
2019-07-23 21:11:07 -0700

    set up first serverless lambda function

---
+++ serverless/libwrapper.py
@@ -0,0 +1,5 @@
+from phishvizlib.helloworld import print_hello_world
+
+
+def hello_world(event, context):
+    print_hello_world()
---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+import setuptools
+
+setup(
+    name='phishvizlib',
+    version='0.0.1',
+    packages=setuptools.find_packages(),
+    long_description=open('README.md').read()
+)
+
+
