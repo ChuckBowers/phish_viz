2d96d5ef59145ee377191e38de9d777f4af7af05
school <josiahc@uw.edu>
2019-08-07 20:53:25 -0700

    drafted some functions to interact with the phish-net api

--- phishvizlib/helloworld.py
+++ phishvizlib/helloworld.py
@@ -1,5 +1,14 @@
-# def print_hello_world():
-#     print('hello world')
+import json
 import requests
 
-requests.get()
+
+def get_show(base_url, api_key):
+    response = requests.get(base_url + "shows/query", params={"apikey": api_key, "year": 2019, "month": 7, "day": 14})
+    content = json.loads(response._content)
+    print(json.dumps(content, indent=4))
+
+
+def get_setlist(base_url, api_key):
+    response = requests.get(base_url + "setlists/get", params={"apikey": api_key, "showid": 1547482678})
+    content = json.loads(response._content)
+    print(json.dumps(content, indent=4))
